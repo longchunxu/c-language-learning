4708cc3dfc7052aa679b6b4003ca8d93e62149df
lcx <792325586@qq.com>
2021-06-13 12:43:08 +0800

    learing file

---
+++ macro_funtion.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define PRINT(x) { printf("hhh\n");\
+printf("%d\n",x*x*x); } 
+#define max(a) { printf("%d\n",a*a);}
+int main()
+{
+	
+//	max(2);
+//	PRINT(2);
+	double *p;
+	printf("%s%s---%d\n",p,p,sizeof(p));
+ 	
+	exit(0);
+} 
