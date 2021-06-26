29e78c29652112a3616097d74a789b1c33b4668d
longchunxu <792325586@qq.com>
2021-06-26 23:25:57 +0800

    删除多余文件

--- kernel_module/drvC/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/桌面/kerneltwo/hello.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /root/桌面/kerneltwo/hello.ko /root/桌面/kerneltwo/hello.o /root/桌面/kerneltwo/hello.mod.o ;  true
--- kernel_module/drvC/.tmp_versions/hello.mod
+++
@@ -1,3 +0,0 @@
-/root/桌面/kerneltwo/hello.ko
-/root/桌面/kerneltwo/hello.o
-
