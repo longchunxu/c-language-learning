b725b1966fa95064606021981e7e322ccf94c7b1
lcx <792325586@qq.com>
2021-09-21 09:26:11 +0800

    冒泡排序完善

--- c/BableSort.cpp
+++ c/BableSort.cpp
@@ -1,43 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void Sort(int *a)
+void Sort(int *a,int len)
 {
-	
-	printf("a=%d",sizeof(a));
-	
-	
-//		for(int i=0;i<sizeof(a)/sizeof(a[0])-1;i++)
-//		for(int j=0;j<sizeof(a)/sizeof(a[0])-i-1;j++)
-//		{
-//			int temp;
-//			if(a[j+1]<a[j])	
-//			{
-//				temp=a[j];
-//				a[j]=a[j+1];
-//				a[j+1]=temp;
-//			}
-//			
-//				
-//			
-//		}	
-//		for(int i=0;i<sizeof(a)/sizeof(a[0]);i++)
-//		printf("a[%d]=%d\n",i,a[i]);
-//	
-//	
-
-	
+		for(int i=0;i<len-1;i++)
+		for(int j=0;j<len-i-1;j++)
+		{
+			int temp;
+			if(a[j+1]<a[j])	
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}		
+		}	
+		for(int i=0;i<len;i++)
+		printf("a[%d]=%d\n",i,a[i]);
 } 
 
 
-int main()
+int main(void)
 {
 	int arr[8]={7,4,0,9,2,1,4,3};	
-	printf("arr=%d\n",sizeof(arr));
-	
-	Sort(arr);
-
-
-
+	Sort(arr,sizeof(arr)/sizeof(int));
 	exit(0);
 }
